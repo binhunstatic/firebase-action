c9450ff7f76be0be3d4b72e2aeedf3d3943c523d
Binh Nguyen Thanh <thanhbinh01234@gmail.com>
2022-08-18 16:54:27 +0700

    Using bash instead of sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,25 @@
 set -e
 
 if [ -z "$FIREBASE_TOKEN" ] && [ -z "$GCP_SA_KEY" ]; then
-  echo "Either FIREBASE_TOKEN or GCP_SA_KEY is required to run commands with the firebase cli"
-  exit 126
+    echo "Either FIREBASE_TOKEN or GCP_SA_KEY is required to run commands with the firebase cli"
+    exit 126
 fi
 
 if [ -n "$GCP_SA_KEY" ]; then
-  if echo "$GCP_SA_KEY" | jq empty 2>/dev/null; then
-    echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
-    echo "$GCP_SA_KEY" > /opt/gcp_key.json
-  else
-    echo "Storing the decoded GCP_SA_KEY in /opt/gcp_key.json"
-    echo "$GCP_SA_KEY" | base64 -d > /opt/gcp_key.json # If encoded base64 key, decode and save
-  fi
+    if echo "$GCP_SA_KEY" | jq empty 2>/dev/null; then
+        echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
+        echo "$GCP_SA_KEY" >/opt/gcp_key.json
+    else
+        echo "Storing the decoded GCP_SA_KEY in /opt/gcp_key.json"
+        echo "$GCP_SA_KEY" | base64 -d >/opt/gcp_key.json # If encoded base64 key, decode and save
+    fi
 
-  echo "Exporting GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json"
-  export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
+    echo "Exporting GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json"
+    export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
 fi
 
 if [ -n "$PROJECT_PATH" ]; then
-  cd "$PROJECT_PATH"
+    cd "$PROJECT_PATH"
 fi
 
 if [ -n "$PROJECT_ID" ]; then
@@ -34,7 +34,7 @@ if [ -n "$CONFIG_VALUES" ]; then
     firebase functions:config:set "$CONFIG_VALUES"
 fi
 
-sh -c "firebase $*"
+/bin/bashs -c "firebase $*"
 
 # response=$(firebase $*)
 
